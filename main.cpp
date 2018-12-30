1f8ce423b320ecb872e6b9294f9377ae0f431c6c
Anthony_Arrowood <arrowood.aj@ufl.edu>
2018-12-30 11:35:37 -0500

    added error log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtGlobal>
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *pFile= fopen("appOutput.log", "w");
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    fprintf(pFile, "%s", localMsg.constData());
+    fflush(pFile);
+
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput); // Install the handler
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
+
+    fclose (pFile);
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -165,9 +165,17 @@ void MainWindow::showRequest(const QString &req)
 
 
         // Show the current values from the port in the current parameters area
-        ui->avgerrLabel->setNum(inputs[i_avg_err]);
-        ui->inputVarLabel->setNum(inputs[i_input_var]);
-        ui->scoreLabel->setNum(inputs[i_score]);
+
+        float show_error_and_inputVar_time = 12.0; // time after which we want to show average error and input variance
+        float show_score_time = 29.0; // time after which we show the score
+        if( inputs[i_time] > show_error_and_inputVar_time ) // only show the input variance after input exclusion time
+        {
+            ui->avgerrLabel->setNum(inputs[i_avg_err]);
+            ui->inputVarLabel->setNum(inputs[i_input_var]);
+        }
+        if ( inputs[i_time] > show_score_time )
+            ui->scoreLabel->setNum(inputs[i_score]); // todo: should only show this after
+
 
         /*
          * After 29 minutes we show the score
