230682c19ef3cc97055b12203a25322537cb4c0e
Anthony <arrowood.aj@ufl.edu>
2019-05-14 21:39:39 -0700

    changed appOutput.log to .log

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE *pFile= fopen("appOutput.log", "w");
+FILE *pFile= fopen(".log", "w");
 
 void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
